78aaf680574a0e0487f2a906368b16aa34c7992f
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-05-22 18:18:20 +0530

    Create index.php

---
+++ web/install/wordpress/index.php
@@ -0,0 +1,36 @@
+<?php
+//Install WordPress
+//
+// Main include
+
+error_reporting(NULL);
+
+ob_start();
+$TAB = 'Install WordPress';
+
+include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+
+// Check user
+if ($_SESSION['user'] != 'admin') {
+    header('Location: /list/user');
+    exit;
+}
+
+/*
+exec (VESTA_CMD . "v-list-user ".$user." json", $outputi, $return_vari);
+$datai = json_decode(implode('', $outputi), true);
+$dati = array_reverse($datao,true);
+$email=$dati["$user"]['CONTACT'];
+print_r($email);*/
+
+exec (VESTA_CMD."v-list-web-domains $user json", $output, $return_var);
+$data = json_decode(implode('', $output), true);
+$data = array_reverse($data,true);
+
+
+
+// Render page
+render_page($user, $TAB, 'install_wp');
+
+// Back uri
+$_SESSION['back'] = $_SERVER['REQUEST_URI'];
