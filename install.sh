7a798a9f9af47f1caeffeb7ca0a2ea0d04ef1b9c
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-05-22 11:33:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "This will install VeataCP Wordpress Installer in your VestaCP"
 # Install WP CLI
 
 # Install Installer
+https://github.com/maskoid/vestacp-wordpress-installer/blob/master/bin/v-install-wordpress
 
 # Install FrontEnd
 
