4769436ca11357a576811597741227034c4e2c4e
Maskoid Technologies <46883634+maskoid@users.noreply.github.com>
2019-05-22 13:45:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ echo "This will install VeataCP Wordpress Installer in your VestaCP"
 cd /usr/local/vesta/bin
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/vesta/bin/wp
 
 # Install Installer
 https://github.com/maskoid/vestacp-wordpress-installer/blob/master/bin/v-install-wordpress
