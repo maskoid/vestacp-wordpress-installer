4d5cd73bc2368183ef95285718eade9ac83975e7
Maskoid <contact@maskoid.com>
2019-05-23 11:47:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,34 +3,38 @@
 
 echo "This will install VeataCP Wordpress Installer in your VestaCP"
 
-# Install WP CLI
-cd /usr/local/vesta/bin
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-#All users will use same folder for cache. Good when there are 100s of users on single server.
-echo "WP_CLI_CACHE_DIR=/home/admin/.wp-cli/cache" >> /etc/environment
-source /etc/environment
+# Check if WP CLI is Installed // Install WP CLI
+wpcli=/usr/local/vesta/bin/wp-cli.phar
+if test -f "$wpcli"; then
+	echo "WP-CLI already installed."
+else
+	cd /usr/local/vesta/bin
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	#All users will use same folder for cache. Good when there are 100s of users on single server.
+	echo "WP_CLI_CACHE_DIR=/home/admin/.wp-cli/cache" >> /etc/environment
+	source /etc/environment
+fi
 
-# Install Installer
-https://github.com/maskoid/vestacp-wordpress-installer/blob/master/bin/v-install-wordpress
-
-Change MOde to 755
-Make it executable
-chmod +x v-install-wordpress
-
-# Install FrontEnd
+# Install VestaCP Wordpress Application Installer
 
 mkdir /usr/local/vesta/web/install
 mkdir /usr/local/vesta/web/install/wordpress
+
+cd /usr/local/vesta/bin
+curl -O https://raw.githubusercontent.com/maskoid/vestacp-wordpress-installer/master/bin/v-install-wordpress
+chmod 755 v-install-wordpress
+chmod +x v-install-wordpress
+
 cd /usr/local/vesta/web/install/wordpress
 curl -O https://raw.githubusercontent.com/maskoid/vestacp-wordpress-installer/master/web/install/wordpress/index.php
-
-# Chmod files 
-chmod 755 /usr/local/vesta/web/list/wp  
-chmod 644 /usr/local/vesta/web/list/wp/index.php
-chmod 644 /usr/local/vesta/web/list/wp/api.php
-
-# Add to Navigation
+cd /usr/local/vesta/web/templates/admin
+curl -O https://raw.githubusercontent.com/maskoid/vestacp-wordpress-installer/master/web/templates/admin/install_wp.html
+# Add to Navigation Admin User
+curl -O https://raw.githubusercontent.com/maskoid/vestacp-wordpress-installer/master/web/templates/admin/panel.html
+# Add to Navigation Normal User
+cd /usr/local/vesta/web/templates/user
+curl -O https://raw.githubusercontent.com/maskoid/vestacp-wordpress-installer/master/web/templates/user/panel.html
 
 # Add the link to the panel.html file 
 if grep -q 'WP' /usr/local/vesta/web/templates/admin/panel.html; then
